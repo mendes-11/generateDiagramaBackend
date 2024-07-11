51eb0ed7e2ef9f762185828ed0c99de3d351baa4
Renato Mendes <rnt.mendes11@gmail.com>
2024-07-11 11:11:48 -0300

    User don't find in database

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,6 @@
-const User = require("../model/user");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
 const { cpf: cpfValidator } = require('cpf-cnpj-validator');
 const isValidCep = require("@brazilian-utils/is-valid-cep");
 
@@ -26,10 +26,10 @@ class UserController {
             const cpfExist = await User.findOne({ cpf: cpf });
             if (cpfExist) return res.status(422).json({ message: "CPF já cadastrado" });
 
-            
+
             const edvExist = await User.findOne({ edv: edv });
             if (edvExist) return res.status(422).json({ message: "EDV já cadastrado" });
-            
+
             const hashedCpf = await bcrypt.hash(cpf, 10);
             const hashedEdv = await bcrypt.hash(edv, 10);
             const hashedCep = await bcrypt.hash(cep, 10);
@@ -57,33 +57,71 @@ class UserController {
         }
     }
 
+    // static async login(req, res) {
+    //     try {
+    //         const { edv, password } = req.body;
+
+    //         if (!edv || !password) return res.status(400).json({ message: "EDV e senha são obrigatórios" });
+
+    //         const isEdvValide = await bcrypt.compare({ edv: edv });
+    //         if (!isEdvValide) return res.status(404).json({ message: "Usuário não encontrado" });
+
+    //         const isPasswordValid = await bcrypt.compare(password, password);
+    //         if (!isPasswordValid) return res.status(401).json({ message: "Senha inválida" });
+
+    //         const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '24h' });
+
+    //         res.status(200).json({ message: "Login bem-sucedido", token: token });
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).send({ message: "Erro ao fazer login", error: error.message });
+    //     }
+    // }
+
+
+
     static async login(req, res) {
         try {
             const { edv, password } = req.body;
-
-            if (!edv || !password) return res.status(400).json({ message: "EDV e senha são obrigatórios" });
-
-            const user = await User.findOne({ edv: edv });
-            if (!user) return res.status(404).json({ message: "Usuário não encontrado" });
-
+    
+            if (!edv || !password) {
+                return res.status(400).json({ message: "EDV e senha são obrigatórios" });
+            }
+    
+            const hashedEdv = await bcrypt.hash(edv, 10);
+    
+            const user = await User.findOne({ edv: hashedEdv });
+            if (!user) {
+                return res.status(404).json({ message: "Usuário não encontrado" });
+            }
+    
+            const isEdvValid = await bcrypt.compare(edv, user.edv);
+            if (!isEdvValid) {
+                return res.status(401).json({ message: "EDV inválido" });
+            }
+    
             const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) return res.status(401).json({ message: "Senha inválida" });
-
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: "Senha inválida" });
+            }
+    
             const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '24h' });
-     
-            res.status(200).json({ message: "Login bem-sucedido", token: token });
+    
+            res.status(200).json({ message: "Login bem-sucedido", token });
+    
         } catch (error) {
             console.error(error);
-            res.status(500).send({ message: "Erro ao fazer login", error: error.message });
+            res.status(500).json({ message: "Erro ao fazer login", error: error.message });
         }
     }
 
-    static async getUser(req, res){
+
+    static async getUser(req, res) {
         const { id } = req.query;
-        try{
+        try {
             const user = await User.findById(id);
-            if(!user) return res.status(404).json({ message: "Usuário não encontrado" })
-            res.status(200).json(user); 
+            if (!user) return res.status(404).json({ message: "Usuário não encontrado" })
+            res.status(200).json(user);
         } catch (error) {
             return res.status(500).send({ menssage: "Erro ao retornar o usuário", error: error.menssage });
         }
@@ -93,38 +131,38 @@ class UserController {
     static async updateUser(req, res) {
         const { id } = req.query;
         const updateFields = req.body;
-    
+
         try {
-            
+
             if (!id) return res.status(400).json({ message: "É necessário fornecer um ID para atualizar o usuário" });
-    
+
             const user = await User.findById(id);
             if (!user) return res.status(404).json({ message: "Usuário não encontrado" });
             // ATUALIZA ITEM POR ITEM
             Object.keys(updateFields).forEach(key => {
                 user[key] = updateFields[key];
             });
-    
+
             await user.save();
-    
+
             res.status(200).json({ message: "Usuário atualizado com sucesso", updatedUser: user });
-    
+
         } catch (error) {
             res.status(500).send({ message: "Erro ao atualizar usuário", error: error.message });
         }
     }
-    
 
-    static async deleteUser(req, res){
+
+    static async deleteUser(req, res) {
         const { id } = req.query;
-        try{
+        try {
             const userID = await User.findById(id);
-            if(!userID) return res.status(404).json({ message: "Usuário não encontrado" });
+            if (!userID) return res.status(404).json({ message: "Usuário não encontrado" });
 
             await User.findByIdAndDelete(id);
             res.status(201).send({ message: "Usuário deletado", userID });
 
-        } catch (error){
+        } catch (error) {
             return res.status(500).send({ message: "Erro ao deletar usuário", error: error.message });
         }
     }
