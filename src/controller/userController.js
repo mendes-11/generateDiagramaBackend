e070704a594bd31e81d71c16d6e64a752e3b61af
Renato Mendes <rnt.mendes11@gmail.com>
2024-07-12 09:02:24 -0300

    login successful

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -8,13 +8,15 @@ class UserController {
     static async register(req, res) {
         try {
             const { name, email, cpf, edv, cep, street, number, password, confirmPassword } = req.body;
-            // VEREFICA SE ELES FORAM INSERIDOS NO BODY.
+
+            // Verifica se todos os campos obrigatórios foram inseridos
             const requiredFields = ['name', 'email', 'cpf', 'edv', 'cep', 'street', 'number', 'password', 'confirmPassword'];
             for (let field of requiredFields) {
                 if (!req.body[field]) {
                     return res.status(400).json({ message: `${field} é obrigatório` });
                 }
             }
+
             if (!isValidEmail(email)) return res.status(400).json({ message: "Email inválido" });
             if (!cpfValidator.isValid(cpf)) return res.status(400).json({ message: "CPF inválido" });
             if (!isValidCep(cep)) return res.status(400).json({ message: "CEP inválido" });
@@ -26,25 +28,19 @@ class UserController {
             const cpfExist = await User.findOne({ cpf: cpf });
             if (cpfExist) return res.status(422).json({ message: "CPF já cadastrado" });
 
-
             const edvExist = await User.findOne({ edv: edv });
             if (edvExist) return res.status(422).json({ message: "EDV já cadastrado" });
 
-            const hashedCpf = await bcrypt.hash(cpf, 10);
-            const hashedEdv = await bcrypt.hash(edv, 10);
-            const hashedCep = await bcrypt.hash(cep, 10);
-            const hashedStreet = await bcrypt.hash(street, 10);
-            const hashedNumber = await bcrypt.hash(number, 10);
             const hashedPassword = await bcrypt.hash(password, 10);
 
             const newUser = new User({
                 name,
                 email,
-                cpf: hashedCpf,
-                edv: hashedEdv,
-                cep: hashedCep,
-                street: hashedStreet,
-                number: hashedNumber,
+                cpf,
+                edv,
+                cep,
+                street,
+                number,
                 password: hashedPassword,
                 createdAt: Date.now()
             });
@@ -57,58 +53,21 @@ class UserController {
         }
     }
 
-    // static async login(req, res) {
-    //     try {
-    //         const { edv, password } = req.body;
-
-    //         if (!edv || !password) return res.status(400).json({ message: "EDV e senha são obrigatórios" });
-
-    //         const isEdvValide = await bcrypt.compare({ edv: edv });
-    //         if (!isEdvValide) return res.status(404).json({ message: "Usuário não encontrado" });
-
-    //         const isPasswordValid = await bcrypt.compare(password, password);
-    //         if (!isPasswordValid) return res.status(401).json({ message: "Senha inválida" });
-
-    //         const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '24h' });
-
-    //         res.status(200).json({ message: "Login bem-sucedido", token: token });
-    //     } catch (error) {
-    //         console.error(error);
-    //         res.status(500).send({ message: "Erro ao fazer login", error: error.message });
-    //     }
-    // }
-
-
-
     static async login(req, res) {
         try {
             const { edv, password } = req.body;
-    
-            if (!edv || !password) {
-                return res.status(400).json({ message: "EDV e senha são obrigatórios" });
-            }
-    
-            const hashedEdv = await bcrypt.hash(edv, 10);
-    
-            const user = await User.findOne({ edv: hashedEdv });
-            if (!user) {
-                return res.status(404).json({ message: "Usuário não encontrado" });
-            }
-    
-            const isEdvValid = await bcrypt.compare(edv, user.edv);
-            if (!isEdvValid) {
-                return res.status(401).json({ message: "EDV inválido" });
-            }
-    
+
+            if (!edv || !password) return res.status(400).json({ message: "EDV e senha são obrigatórios" });
+
+            const user = await User.findOne({ edv: edv });
+            if (!user) return res.status(404).json({ message: "Usuário não encontrado" });
+
             const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) {
-                return res.status(401).json({ message: "Senha inválida" });
-            }
-    
+            if (!isPasswordValid) return res.status(401).json({ message: "Senha inválida" });
+
             const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '24h' });
-    
+
             res.status(200).json({ message: "Login bem-sucedido", token });
-    
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: "Erro ao fazer login", error: error.message });
